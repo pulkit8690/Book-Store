9ba77f921371e788a2e3c98ae227ab08d34cd87e
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2025-04-16 14:44:21 +0530

    commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -44,7 +44,7 @@ const Footer = () => {
             <br />
             <div className="flex items-center gap-3">
               <FaLocationArrow />
-              <p>Noida, Uttar Pradesh</p>
+              <p>Ludhiana, Punjab</p>
             </div>
             <div className="flex items-center gap-3 mt-3">
               <FaMobileAlt />
@@ -115,7 +115,7 @@ const Footer = () => {
         </div>
         <div>
           <div className="text-center py-10 border-t-2 border-gray-300/50">
-            @copyright 2024 All rights reserved || Made with ❤️ by Dilshad
+            @copyright 2024 All rights reserved || Made with ❤️ by Pulkit Arora
           </div>
         </div>
       </section>
